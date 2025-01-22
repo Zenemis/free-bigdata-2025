d406ac9a3797d58e41752e93d9d54957e70d5ea2
Simon DUCAMP <ducampsimon@outlook.fr>
2025-01-22 16:47:25 +0100

    revert: old get raw battles

--- src/main/java/sparkwinrate/DataReader.java
+++ src/main/java/sparkwinrate/DataReader.java
@@ -1,11 +1,15 @@
 package sparkwinrate;
 
 import java.time.Instant;
+import java.util.ArrayList;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
 import datacleaner.Battle;
+import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import scala.Tuple2;
 
 
 public class DataReader {
@@ -24,18 +28,25 @@ public class DataReader {
         this.weeks = 9;
     }
 
-    public JavaRDD<Battle> getDistinctBattles(JavaSparkContext sc) {
-        // Lecture des fichiers JSON Hadoop dans le dossier ./it360/clean-big.txt
-        JavaRDD<String> rdd = sc.textFile(path).filter((x) -> !x.isEmpty() && !x.equals("test"));
+    public static JavaRDD<Battle> getDistinctRawBattles(JavaSparkContext sc) {
+        int weeks = WEEKS;
 
-        // Transformation des JSON en objets Battle
-        ObjectMapper mapper = new ObjectMapper();
-        JavaRDD<Battle> rddpair = rdd.map((x) ->
-            mapper.readValue(x, Battle.class)
-        );
+        JavaRDD<String> rdd = sc.textFile("./data_ple/clashroyale2024/clash_big.nljson").filter((x) -> {
+            return !x.isEmpty();
+        });
+        //System.out.println("battles " + rdd.count());
 
-        // Filtrage des batailles selon la fenêtre temporelle
-        Instant sliding_window = Instant.now().minusSeconds(3600L * 24 * 7 * weeks);
+        // distinct battles
+        JavaRDD<Battle> rddpair = rdd.mapToPair((x) -> {
+            Gson gson = new Gson();
+            Battle d = gson.fromJson(x, Battle.class);
+            String u1 = d.players.get(0).utag;
+            String u2 = d.players.get(1).utag;
+            return new Tuple2<>(d.date + "_" + d.round + "_"
+                    + (u1.compareTo(u2) < 0 ? u1 + u2 : u2 + u1), d);
+        }).distinct().values();
+
+        Instant sliding_window = Instant.now().minusSeconds(3600 * 24 * 7 * weeks);
         Instant collect_start = Instant.parse("2024-09-26T09:00:00Z");
 
         rddpair = rddpair.filter((Battle x) -> {
@@ -43,6 +54,38 @@ public class DataReader {
             return inst.isAfter(sliding_window) && inst.isAfter(collect_start);
         });
 
-        return rddpair;
+        JavaPairRDD<String, Iterable<Battle>> rddbattles = rddpair.mapToPair((d) -> {
+            String u1 = d.players.get(0).utag;
+            String u2 = d.players.get(1).utag;
+            double e1 = d.players.get(0).elixir;
+            double e2 = d.players.get(1).elixir;
+            return new Tuple2<>(d.round + "_"
+                    + (u1.compareTo(u2) < 0 ? u1 + e1 + u2 + e2 : u2 + e2 + u1 + e1), d);
+        }).groupByKey();
+
+        JavaRDD<Battle> clean = rddbattles.values().flatMap((it) -> {
+            ArrayList<Battle> lbattles = new ArrayList<>();
+            ArrayList<Battle> rbattles = new ArrayList<>();
+            for (Battle bi : it)
+                lbattles.add(bi);
+            lbattles.sort((Battle x, Battle y) -> {
+                if (x.date.isAfter(y.date))
+                    return 1;
+                if (y.date.isAfter(x.date))
+                    return -1;
+                return 0;
+            });
+            rbattles.add(lbattles.get(0));
+            for (int i = 1; i < lbattles.size(); ++i) {
+                long i1 = lbattles.get(i - 1).date.getEpochSecond();
+                long i2 = lbattles.get(i).date.getEpochSecond();
+                if (Math.abs(i1 - i2) > 10)
+                    rbattles.add(lbattles.get(i));
+            }
+            return rbattles.iterator();
+        });
+
+        return clean;
     }
+
 }
